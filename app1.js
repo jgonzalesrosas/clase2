e5e0c80135910fdd4ba9fc8a1334228eb64678fa
jgonzalesrosas <jgonzalesdev@yahoo.com>
2017-12-16 11:24:05 -0500

    tercera modificacion

--- app1.js
+++ app1.js
@@ -1,9 +1,24 @@
-var http = require('http');
+/*var http = require('http');
 var puerto = 3000;
 http.createServer(function(req, res){
 	res.writeHead(200);
 	res.write("Hola mundo\n");
-	red.end();
+	res.end();
 }).listen(puerto);
 
+console.log("Estamos escuchando en el puerto", puerto);
+*/
+
+var http = require('http');
+var puerto = 3000;
+
+var manejador = function(req, res){
+	res.writeHead(200);
+	console.log("Esta respuesta solo lo ve el servidor");
+	res.write('Hola mundo\n');
+	res.end();
+}
+
+var servidor = http.createServer(manejador);
+servidor.listen(puerto);
 console.log("Estamos escuchando en el puerto", puerto);
