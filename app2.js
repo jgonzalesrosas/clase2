95cea925af287c58751b8fcaad08c247c15e42d6
jgonzalesrosas <jgonzalesdev@yahoo.com>
2017-12-16 12:09:31 -0500

    app2 verion 2

--- app2.js
+++ app2.js
@@ -1,13 +1,27 @@
-var http = require('http');
-var puerto = 3000;
+//var http = require('http');
+//var puerto = 3000;
 
-http.createServer(function(req, res)){
+//http.createServer(function(req, res){
+//	res.writeHead(200);
+//	res.write('Hola mundo');
+//	setTimeout(function(){
+//		res.end();
+//		console.log("El proceso termino");
+//	}, 5000);
+//}).listen(puerto);
+//
+//console.log("Estamos escuchando en el puerto", puerto);
+
+var http = require ('http');
+var puerto = 8080;
+
+http.createServer(function(req, res){
 	res.writeHead(200);
-	res.write('Hola mundo');
-	setTimeout(function(){
-		res.end();
-		console.log("El proceso termino");
-	}, 5000);
+	res.write('Mandamos a procesar');
+	setTimeout (function(){
+		console.log("Procesamos un archivo pesado")
+	},10000);
+	res.end();
 }).listen(puerto);
 
-console.log("Estamos escuchando en el puerto", puerto);
+console.log("EScuchando en el puerto", puerto);
